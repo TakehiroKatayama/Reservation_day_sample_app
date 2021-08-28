f003af2f8e2d00d815e071973f2ffc5bf42715db
TakehiroKatayama <t.katayama.info@gmail.com>
2021-08-28 20:27:20 +0900

    statusカラムを追加しenumを定義

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,4 +1,10 @@
 class Reservation < ApplicationRecord
   belongs_to :day
   belongs_to :user
+
+  enum status: {
+    visiting: 0,
+    visited: 1,
+    cancel: 2
+  }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_27_080132) do
+ActiveRecord::Schema.define(version: 2021_08_28_111024) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,6 +37,7 @@ ActiveRecord::Schema.define(version: 2021_08_27_080132) do
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "user_id", null: false
     t.string "email"
+    t.integer "status", default: 0
     t.index ["day_id"], name: "index_reservations_on_day_id"
     t.index ["user_id"], name: "index_reservations_on_user_id"
   end
